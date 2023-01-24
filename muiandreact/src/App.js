4571cbc35955873d341b1776ff1bc4d8c22c41cb
Mystereyn <teukureynaldi106@gmail.com>
2023-01-24 22:54:25 +0700

    Fixing much  errors and adding some parents and child  div

--- muiandreact/src/App.js
+++ muiandreact/src/App.js
@@ -1,11 +1,27 @@
-import CustomeTheme from "./components/ThemeMUI";
-
+import Button from "@mui/material/Button";
+import Box from "@mui/material/Box";
 function App() {
   return (
-    <ThemeProvider theme={ThemeMUI}>
-      <CssBaseline />
-      <main> Testing theme</main>
-    </ThemeProvider>
+    <div className="App">
+      <div className= "btn">
+          <Button variant="contained"> 
+              this is a contained Button
+          </Button>
+      </div>
+      <div className="box">
+      <Box
+      sx={{
+        width: 300,
+        height: 300,
+        backgroundColor: 'primary.dark',
+        '&:hover': {
+          backgroundColor: 'primary.main',
+          opacity: [0.9, 0.8, 0.7],
+        },
+      }}
+    />
+      </div>
+    </div>
   );
 }
 
