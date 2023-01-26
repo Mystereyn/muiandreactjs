7c3f0f96e0e7ac7307b61f9540ac1bef69890ead
Mystereyn <teukureynaldi106@gmail.com>
2023-01-26 22:53:25 +0700

    commit

---
+++ muiandreact/src/components/Footer.jsx
@@ -0,0 +1,20 @@
+// https://www.devwares.com/docs/contrast/react/components/footer/
+
+import React from 'react';
+import {
+    CDBFooter,
+    CDBFooterLink, 
+    CDBContainer,
+    CDBBtn,
+    CDBIcon
+} from "cdbreact";
+
+export function customizeFooter()  {
+    return (
+        <CDBFooter className="shadow">
+            <CDBBox 
+            display="flex"
+            justifyContent=></CDBBox>
+        </CDBFooter>
+    )
+}
