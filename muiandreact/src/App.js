7433de6b81384fe9ec5edf6ef9b4edf350d60af9
Mystereyn <teukureynaldi106@gmail.com>
2023-01-26 15:12:23 +0700

    something has been big changed

--- muiandreact/src/App.js
+++ muiandreact/src/App.js
@@ -1,5 +1,9 @@
 import Button from "@mui/material/Button";
 import Box from "@mui/material/Box";
+import MyPict from "./Picts/myPict.jpg";
+
+import React from "react";
+
 function App() {
   return (
     <div className="App">
@@ -8,19 +12,16 @@ function App() {
               this is a contained Button
           </Button>
       </div>
-      <div className="box">
-      <Box
-      sx={{
-        width: 300,
-        height: 300,
-        backgroundColor: 'primary.dark',
-        '&:hover': {
-          backgroundColor: 'primary.main',
-          opacity: [0.9, 0.8, 0.7],
-        },
-      }}
-    />
+      <div className="img">
+        <img src={MyPict} 
+            height="50%" 
+            alt="myPict" 
+            width="50%">
+        </img>
       </div>
+      <h1 className="text-3xl font-bold underline">
+      Hello world!
+      </h1>
     </div>
   );
 }
